a93b062ba9fe5f5e6689dc30d7cc7cb3f5c1625f
cobie1235 <cobie@usf.edu>
2022-01-19 15:46:15 -0500

    Commit the changes done in this file.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("The world is only here to challenge you and without effort, you cant say you really tried!");
+﻿// Program Developed by: Cobie Luna
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("The world is only here to challenge you and without effort, you cant say you really tried!"); // Custom Message when application activates
+
+Console.WriteLine("The world is here for everyone but you have to be willing to change!"); // Custom message for bot testing
